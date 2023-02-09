41c07483a5cf74ef16510b6d7ec31c474a9a4603
Gabriel Shannon <gabrielshannon96@gmail.com>
2023-02-09 21:36:36 +0000

    test commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,135 +1,131 @@
 export const mediaItems = [
-  
+  {
+    item: 'SLUMP SOUNDS 01.02.22 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/dGJZMzn8Lvs?autoplay=0',
+    audio: 0,
+  },
 
-      {
-        item: 'SLUMP SOUNDS 01.02.22 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/dGJZMzn8Lvs?autoplay=0',
-        audio: 0,
-      },
+  {
+    item: 'SLUMP SOUNDS 23.11.21 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/Sei7gYCnHEQ?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 12.10.21 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/MGVzGE_oYTI?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 07.09.21 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/qW4CQNdpza0?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 03.08.21 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/iI9Zjg0i18M?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS // LATITUDE FESTIVAL',
+    data: 'https://www.youtube.com/embed/zxBWis3Jixw?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 22.06.21 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/6jKAixyOUno?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 20.04.21 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/HWW8NQEV2fg?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 03.11.20 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/0IWsJWxv7Ok?autoplay=0',
+    audio: 0,
+  },
+  {
+    item: 'SLUMP SOUNDS 29.09.20 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/9w61G-KfKWI?autoplay=0',
+    audio: 0,
+  },
 
-      {
-        item: 'SLUMP SOUNDS 23.11.21 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/Sei7gYCnHEQ?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 12.10.21 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/MGVzGE_oYTI?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 07.09.21 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/qW4CQNdpza0?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 03.08.21 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/iI9Zjg0i18M?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS // LATITUDE FESTIVAL',
-        data: 'https://www.youtube.com/embed/zxBWis3Jixw?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 22.06.21 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/6jKAixyOUno?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 20.04.21 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/HWW8NQEV2fg?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 03.11.20 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/0IWsJWxv7Ok?autoplay=0',
-        audio: 0,
-      },
-      {
-        item: 'SLUMP SOUNDS 29.09.20 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/9w61G-KfKWI?autoplay=0',
-        audio: 0,
-      },
+  {
+    item: 'Rotation w Noah Tucker - Slump Sounds 19.08.20 // Vandelay Radio',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Rotation-w-Noah-Tucker-Slump-Sounds-Vandelay-Radio.mp3',
+  },
 
-      {
-        item: 'Rotation w Noah Tucker - Slump Sounds 19.08.20 // Vandelay Radio',
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Rotation-w-Noah-Tucker-Slump-Sounds-Vandelay-Radio.mp3',
-      },
+  {
+    item: 'SLUMP SOUNDS 04.08.20 // KINDRED RADIO',
+    data: 'https://www.youtube.com/embed/8tClTQ_GWV8?autoplay=0',
+    audio: 0,
+  },
 
-      {
-        item: 'SLUMP SOUNDS 04.08.20 // KINDRED RADIO',
-        data: 'https://www.youtube.com/embed/8tClTQ_GWV8?autoplay=0',
-        audio: 0,
-      },
+  {
+    item: 'Slump Sounds Presents... Valentines Special w_ Curbside 14.02.2019 // Kmah Radio',
+    data: null,
+    audio:
+      'http://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-Valentines-Special-w_-Curbside-Kmah-Radio-14.02.2019.mp3',
+  },
 
-      {
-        item: 'Slump Sounds Presents... Valentines Special w_ Curbside 14.02.2019 // Kmah Radio',
-        data: null,
-        audio:
-          'http://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-Valentines-Special-w_-Curbside-Kmah-Radio-14.02.2019.mp3',
-      },
+  {
+    item: 'Slump Sounds Presents... 2 HOUR SPECIAL VOL.1 05.07.2019 // Kmah Radio',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-2-HOUR-SPECIAL-VOL.1-Kmah-Radio-05.07.2019.mp3',
+  },
 
-      {
-        item: 'Slump Sounds Presents... 2 HOUR SPECIAL VOL.1 05.07.2019 // Kmah Radio',
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-2-HOUR-SPECIAL-VOL.1-Kmah-Radio-05.07.2019.mp3',
-      },
+  {
+    item: 'Slump Sounds Presents... Baccy Beard b2b Vesarchie 11.04.19 // Kmah Radio',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-Baccy-Beard-b2b-Vesarchie-Kmah-Radio-11.04.19.mp3',
+  },
 
-      {
-        item: 'Slump Sounds Presents... Baccy Beard b2b Vesarchie 11.04.19 // Kmah Radio',
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-Baccy-Beard-b2b-Vesarchie-Kmah-Radio-11.04.19.mp3',
-      },
+  {
+    item: 'Slump Sounds Presents... Holloway 05.06.2019 // Kmah Radio ',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-Holloway-Kmah-Radio-05.06.2019.mp3',
+  },
 
-      {
-        item: 'Slump Sounds Presents... Holloway 05.06.2019 // Kmah Radio ',
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-Holloway-Kmah-Radio-05.06.2019.mp3',
-      },
+  {
+    item: 'Slump Sounds Presents... FAE 14.03.2019 // Kmah Radio',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-FAE-Kmah-Radio-14.03.2019.mp3',
+  },
 
-      {
-        item: 'Slump Sounds Presents... FAE 14.03.2019 // Kmah Radio',
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-FAE-Kmah-Radio-14.03.2019.mp3',
-      },
+  {
+    item: 'Slump Sounds Presents... PROSPA 29_08_2019 // Kmah Radio',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-PROSPA-Kmah-Radio-29_08_2019-1.mp3',
+  },
 
-      {
-        item: 'Slump Sounds Presents... PROSPA 29_08_2019 // Kmah Radio',
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Slump-Sounds-Presents...-PROSPA-Kmah-Radio-29_08_2019-1.mp3',
-      },
+  {
+    item: 'SLUMP SOUNDS PRESENTS: SUMMER SLAM PROMO MIX 18.08.2019 (JOB TYPE)',
 
-      {
-        item: 'SLUMP SOUNDS PRESENTS: SUMMER SLAM PROMO MIX 18.08.2019 (JOB TYPE)',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2021/11/Job-Type-Summer-Slam-tape-1.mp3',
+  },
 
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2021/11/Job-Type-Summer-Slam-tape-1.mp3',
-      },
+  {
+    item: 'SLUMP SOUNDS PRESENTS... DOWN2EARTH 01_08_2019 // Kmah Radio',
 
-      {
-        item: 'SLUMP SOUNDS PRESENTS... DOWN2EARTH 01_08_2019 // Kmah Radio',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2022/03/Slump-Sounds-Presents...-DOWN2EARTH-Kmah-Radio-01_08_2019-1.mp3',
+  },
 
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2022/03/Slump-Sounds-Presents...-DOWN2EARTH-Kmah-Radio-01_08_2019-1.mp3',
-      },
-
-      {
-        item: 'BACCY-BEARD-MINIMIX-2018',
-
-        data: null,
-        audio:
-          'https://slmpbackend8080.online/wp-content/uploads/2022/03/BACCY-BEARD-MINIMIX-2018.mp3',
-      },
-  
-    ]
+  {
+    item: 'BACCY-BEARD-MINIMIX-2018',
+    data: null,
+    audio:
+      'https://slmpbackend8080.online/wp-content/uploads/2022/03/BACCY-BEARD-MINIMIX-2018.mp3',
+  },
+];
