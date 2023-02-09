9bce662b7b7cd8f675deb628b0a2d8f677ee2ab7
gabrielshannonsocius <113363825+gabrielshannonsocius@users.noreply.github.com>
2023-02-09 21:12:00 +0000

    routing and api requests done

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,45 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 // const formatDate = (date) => new Date(date).toLocaleDateString();
 
 export default function PostCard({ post }) {
   const { title, slug, featuredImage } = post;
   
 
-  console.log(slug)
+  console.log(featuredImage)
 
   return (
-    <div className="post-card">
-      {/* <img
-        src={featuredImage.mediaItemURL}
+//     <div className="post-card">
+//       {/* <img
+//         src={featuredImage.mediaItemURL
+// }
        
-      /> */}
-      <Link to={`/blog/${slug}`}>
-        <h3>{title}</h3>
-      </Link>
+//       /> */}
+//       <Link to={`/blog/${slug}`}>
+//         <h3>{title}</h3>
+//       </Link>
+//     </div>
+
+
+
+    <div className="post-card">
+      {featuredImage ? (
+        <img
+        className='featured-image'
+          src={featuredImage.node.mediaItemUrl}
+          alt={featuredImage.node.altText}
+        />
+      ) : null}
+    <div className='menu-item"'>
+
+   
+  <Link to={`/blog/${slug}`}>
+
+    <h3>{title}</h3>
+  </Link> </div>
+      <div className="metadata">
+        <p>
+          {/* <span className="text-bold">Date:</span> {formatDate(date)} */}
+        </p>
+        <p>
+          {/* <span className="text-bold">Author:</span> {authorName} */}
+        </p>
+      </div>
     </div>
   );
-
-  // return (
-  //   <div className="post-card">
-  //     {featuredImage ? (
-  //       <img
-  //         src={featuredImage.node.sourceUrl}
-  //         alt={featuredImage.node.altText}
-  //       />
-  //     ) : null}
-  // <Link to={`/blog/${slug}`}>
-  //   <h3>{title}</h3>
-  // </Link>
-  //     <div className="metadata">
-  //       <p>
-  //         {/* <span className="text-bold">Date:</span> {formatDate(date)} */}
-  //       </p>
-  //       <p>
-  //         {/* <span className="text-bold">Author:</span> {authorName} */}
-  //       </p>
-  //     </div>
-  //   </div>
-  // );
 }
